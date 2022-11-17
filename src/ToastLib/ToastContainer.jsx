a9a13b6a1ada93a5ab2cf08f3a2e04c24454bb9f
sskanishk <kanishkmalviya78@gmail.com>
2022-11-17 08:31:25 +0530

    css fixed and icons added

--- src/ToastLib/ToastContainer.jsx
+++ src/ToastLib/ToastContainer.jsx
@@ -1,5 +1,9 @@
 import useStore from "../store"
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleCheck } from '@fortawesome/free-regular-svg-icons'
+import { faXmark, faExclamationCircle } from '@fortawesome/free-solid-svg-icons'
+
 function ToastContainer({}) {
 
     const toastList = useStore((state) => state.toastList)
@@ -37,8 +41,18 @@ function Toast({toast, position}) {
     
     return (
         <div className={`toast__box ${toast.type}`}>
-            <p>{toast.title}</p>
-            <button className="close_btn" onClick={() => removeToastx(position, toast.id)}>x</button>
+            <div className="toast__info">
+                {
+                    toast.type === 'success'
+                    ? <FontAwesomeIcon icon={faCircleCheck} size={'1x'} onClick={() => removeToastx(position, toast.id)} />
+                    : toast.type === 'error'
+                        ? <FontAwesomeIcon icon={faExclamationCircle} size={'1x'} onClick={() => removeToastx(position, toast.id)} />
+                        : null
+                }
+                <p>{toast.title} </p>
+            </div>
+
+            <FontAwesomeIcon className="close_btn" icon={faXmark} size={'1x'} onClick={() => removeToastx(position, toast.id)} />
         </div>
     )
 }
